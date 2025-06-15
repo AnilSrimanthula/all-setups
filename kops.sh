cfc1a77cb422ae2ece1bb07a9b09b4a0df41e5fa
AnilSrimanthula <anilsrimanthula1999@gmail.com>
2025-06-15 11:46:04 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -10,3 +10,6 @@ wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
+export KOPS_STATE_STORE=s3://anil.flm
+#aws ssm get-parameter --name "/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id" --region us-east-1 --query "Parameter.Value" --output text
+kops create cluster --name anil.k8s.local --zones us-east-1a --master-count=1 --master-size t2.micro --node-count=2 --node-size t2.micro --image ami-054d6a336762e438e
